652f6e59c35c212d67f46a4e849e495329ee38e2
Carlos Calero <calerofuente@gmail.com>
2019-02-26 20:29:37 +0100

    Añadido bootstrap y configurado axios

--- app-front/src/main.js
+++ app-front/src/main.js
@@ -2,7 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
 import router from './router'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
+
+Vue.use(BootstrapVue)
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 Vue.config.productionTip = false
 
@@ -10,6 +24,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   components: { App },
   template: '<App/>'
 })
