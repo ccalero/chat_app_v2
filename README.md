f6e2168659ddc07969551c7b6ef453ca6e792c54
Carlos Calero <calerofuente@gmail.com>
2019-03-04 01:21:12 +0100

    Añadido asistente para la instalacion y fichero README

--- README.md
+++ README.md
@@ -1,24 +1,153 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Chat App v2
 
-Things you may want to cover:
+Aplicación Chat donde se han utilizado las herramientas:
 
-* Ruby version
+* Ruby on Rails 5.2
+* Action Cable
+* Vuejs 2.5
+* Rspec
+* Mongoid 7.0
 
-* System dependencies
+La aplicación tiene un frontend (desarrollado con VueJS) y un backend (desarrollado en RoR). La comunicación se realiza mediante API JSON REST
 
-* Configuration
+## Despliegue automático
 
-* Database creation
+Para esta versión se ha incorporado un asistente que irá preguntando si se quiere instalar los distintos componentes.
 
-* Database initialization
+Se situa en la carpeta raiz "deploy.sh" (solo disponible para linux)
 
-* How to run the test suite
+    $ sh deploy.sh
 
-* Services (job queues, cache servers, search engines, etc.)
+Nota: Ejecutar en la carpeta del proyecto
 
-* Deployment instructions
+## Despliegue manual
 
-* ...
+### Instalamos dependencias git, rbenv y ruby-build
+
+    $ sudo apt-get install git
+    $ sudo apt-get install rbenv
+    $ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+Comprobamos que rbenv se ha instalado correctamente
+
+    $ type rbenv
+
+### Instalación Ruby
+
+Vamos a instalar la versión 2.5.0
+
+    $ rbenv install 2.5.0
+
+Una vez instalado, le decimos al sistema que use esta versión por defecto
+
+    $ rbenv rehash
+    $ rbenv global 2.5.0
+
+### Instalación Rails
+
+Para instalar, utilizamos el comando *gem*, instalaremos la version 5.1.6
+
+    $ gem install rails -v 5.2
+
+### Incluir ~/.rbenv/bin en $PATH:
+
+    $ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    $ echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    $ source ~/.bashrc
+
+### Instalación JWT
+
+JWT depende del servicio redis-server, para instalarlo:
+
+	$ sudo apt-get install -y redis-server
+
+Despues instalamos JWT con gem
+
+    $ gem install jwt
+
+### Preparamos los servicios (MongoDB)
+
+La aplicación está preparada para trabajar con la base de datos de MongoDB.
+
+Instalamos MongoDB en el sistema,
+
+Esto dependerá del sistema que utilices, en mi caso, el sistema que se ha utilizado es Debian 9 64 bits.
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-debian/
+
+    echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+    sudo apt-get update
+
+    sudo apt-get install mongodb-org
+
+Una vez instalado, iniciamos el servicio:
+
+    sudo service mongod start
+
+Comprobamos el estado del servicio
+
+    sudo service mongod status
+
+## Clonamos repositorio
+
+    $ git clone https://github.com/ccalero/chat_app.git
+
+
+## Instalamos las gemas del proyecto
+
+    $ bundle install
+
+Si fuera necesario actualizamos
+
+    $ bundle update
+
+### Instalamos las dependencias de VueJS
+
+  	$ cd app-front/
+  	$ yarn install
+
+## Ejecutamos el servidor:
+
+Una vez hecho todo, ya podemos ejecutar el servidor dentro del directorio "chat_app_v2"
+
+    $ rails s
+
+Y el servidor de vuejs dentro del directorio chat_app_v2/app-front
+
+    $ yarn dev
+
+## Imagenes de la Aplicación
+
+### Página del Login
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/1_SignIn.PNG)
+
+### Página del Registro
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/2_SignUp.PNG)
+
+### Página del listado de salas disponibles
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/3_rooms.PNG)
+
+### Página para crear una nueva sala
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/4_add_room.PNG)
+
+### Página del chat
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/5_chat_room.PNG)
+
+### Control de errores en la aplicación
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/6_control_errors.PNG)
+
+## Demo
+
+Aqui podemos ver, el funcionamiento del chat con dos usuarios ("usuario1" y "usuario2") en la sala "Sala de Demo". Al mismo tiempo otros dos usuarios ("usuario3" y "usuario4") están en la sala "Sala 2"
+
+En ventanas independientes que se actualizan con los nuevos mensajes sin refrescar la página
+
+![alt text](https://github.com/ccalero/chat_app_v2/blob/master/doc/img/demo_chat.gif)
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+# deploy.sh
+# Authors:
+# ---------------------------------------------
+# Carlos Calero
+# https://github.com/ccalero/
+# ---------------------------------------------
+#
+# ---------------------------------------------
+# Usage:
+# ---------------------------------------------
+# To deploy, run:
+# sh deploy.sh
 
-# Variables
 CMD_UPDATE="sudo apt-get update"
 CMD_INSTALL_GIT="sudo apt-get -y install git"
-CMD_INSTALL_RBENV="sudo apt-get -y install rbenv"
-CMD_CLONE_RUBY_BUILD="git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build"
-CMD_INSTALL_RUBY="rbenv install 2.5.0"
-CMD_SET_RUBY_VERSION="rbenv rehash;rbenv global 2.5.0"
-CMD_INSTALL_RAILS="gem install rails -v 5.2"
-CMD_SET_PATH_RBENV="echo 'export PATH=\"\$HOME/.rbenv/bin:\$PATH\"' >> ~/.bashrc; echo 'eval \"\$(rbenv init -)\"' >> ~/.bashrc;source ~/.bashrc"
-CMD_INSTALL_REDIS="sudo apt-get install -y redis-server"
-
 
 echo "\n"
 echo "#############################"
@@ -20,7 +23,7 @@ echo "#############################"
 echo "\n"
 echo "Se necesita instalar git, se ejecutara:"
 echo "$CMD_INSTALL_GIT"
-read -p "Deseas instalar git? (s,n)" install
+read -p "Deseas instalar git? (s,n) " install
 if echo "$install" | grep -iq "^s" ;then
     echo Instalando git...
     echo "--------------------------------"
@@ -30,61 +33,187 @@ else
     echo Omitimos Git
 fi
 
+CMD_INSTALL_RBENV="sudo apt-get -y install rbenv"
+CMD_CLONE_RUBY_BUILD="git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build"
+
 echo "\n"
 echo "Se necesita instalar rbenv, se ejecutara:"
 echo "$CMD_INSTALL_RBENV"
-read -p "Deseas instalar rbenv? (s,n)" install
+echo "$CMD_CLONE_RUBY_BUILD"
+read -p "Deseas instalar rbenv? (s,n) " install
 if echo "$install" | grep -iq "^s" ;then
     echo Instalando rbenv...
     echo "--------------------------------"
-    $CMD_UPDATE;$CMD_INSTALL_RBENV
+    $CMD_UPDATE
+    $CMD_INSTALL_RBENV
+    $CMD_CLONE_RUBY_BUILD
+    if [ $? -ne 0 ]; then
+       echo "[ERROR] Un error ha ocurrido al ejecutar:"
+       echo "$CMD_CLONE_RUBY_BUILD"
+       echo "Pruebe a copiar el comando y a continuar con la instalacion"
+       exit 3
+    fi
     echo "--------------------------------"
 else
     echo Omitimos rbenv
 fi
 
+
+CMD_INSTALL_RUBY="rbenv install 2.5.0"
+CMD_RUBY_REHASH="rbenv rehash"
+CMD_RUBY_GLOBAL="rbenv global 2.5.0"
+
 echo "\n"
 echo "Se necesita instalar ruby 2.5.0, se ejecutara:"
 echo "$CMD_INSTALL_RUBY"
-read -p "Deseas instalar ruby? (s,n)" install
+echo "$CMD_RUBY_REHASH"
+echo "$CMD_RUBY_GLOBAL"
+read -p "Deseas instalar ruby? (s,n) " install
 if echo "$install" | grep -iq "^s" ;then
     echo Instalando ruby...
     echo "--------------------------------"
-    $CMD_INSTALL_RBENV;$CMD_INSTALL_RUBY
-    $CMD_SET_RUBY_VERSION
+    $CMD_INSTALL_RUBY
+    $CMD_RUBY_REHASH
+    $CMD_RUBY_GLOBAL
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    source ~/.bashrc
     echo "--------------------------------"
 else
     echo Omitimos ruby
 fi
 
+CMD_INSTALL_RAILS="gem install rails -v 5.2"
 
 echo "\n"
 echo "Se necesita instalar rails 5.2, se ejecutara:"
 echo "$CMD_INSTALL_RAILS"
 echo "$CMD_SET_PATH_RBENV"
-read -p "Deseas instalar rails? (s,n)" install
+read -p "Deseas instalar rails? (s,n) " install
 if echo "$install" | grep -iq "^s" ;then
-    echo Instalando ruby...
+    echo Instalando rails...
     echo "--------------------------------"
     $CMD_INSTALL_RAILS
+    if [ $? -ne 0 ]; then
+       echo "[ERROR] Un error ha ocurrido al ejecutar:"
+       echo "$CMD_INSTALL_RAILS"
+       echo "Pruebe a copiar el comando y a continuar con la instalacion"
+       exit 3
+    fi
     $CMD_SET_PATH_RBENV
+    if [ $? -ne 0 ]; then
+       echo "[ERROR] Un error ha ocurrido al ejecutar:"
+       echo "$CMD_SET_PATH_RBENV"
+       echo "Pruebe a copiar el comando y a continuar con la instalacion"
+       exit 3
+    fi
     echo "--------------------------------"
 else
     echo Omitimos rails
 fi
 
-# dependencias para JWT
-# echo "\n"
-# echo "Se necesita instalar rails 5.2, se ejecutara:"
-# echo "$CMD_INSTALL_RAILS"
-# echo "$CMD_SET_PATH_RBENV"
-# read -p "Deseas instalar rails? (s,n)" install
-# if echo "$install" | grep -iq "^s" ;then
-#     echo Instalando ruby...
-#     echo "--------------------------------"
-#     $CMD_INSTALL_RAILS
-#     $CMD_SET_PATH_RBENV
-#     echo "--------------------------------"
-# else
-#     echo Omitimos rails
-# fi
+CMD_INSTALL_REDIS="sudo apt-get install -y redis-server"
+CMD_INSTALL_JWT="gem install jwt"
+
+echo "\n"
+echo "Se necesita instalar jwt y sus dependencias, se ejecutara:"
+echo "$CMD_INSTALL_REDIS"
+echo "$CMD_INSTALL_JWT"
+read -p "Deseas instalar jwt? (s,n) " install
+if echo "$install" | grep -iq "^s" ;then
+    echo Instalando jwt...
+    echo "--------------------------------"
+    $CMD_UPDATE
+    $CMD_INSTALL_REDIS
+    $CMD_INSTALL_JWT
+    if [ $? -ne 0 ]; then
+       echo "[ERROR] Un error ha ocurrido al ejecutar:"
+       echo "$CMD_INSTALL_JWT"
+       echo "Pruebe a copiar el comando y a continuar con la instalacion"
+       exit 3
+    fi
+    echo "--------------------------------"
+else
+    echo Omitimos jwt
+fi
+
+CMD_INSTALL_MONGODB="sudo apt-get install -y mongodb-org"
+CMD_INIT_SERVICE_MONGODB="sudo service mongod start"
+
+echo "\n"
+echo "Se necesita instalar mongodb, se ejecutara:"
+echo "$CMD_INSTALL_MONGODB"
+echo "$CMD_INIT_SERVICE_MONGODB"
+read -p "Deseas instalar mongodb? (s,n) " install
+if echo "$install" | grep -iq "^s" ;then
+    echo "ADVERTENCIA"
+    echo "Se necesitan actualizar los repositorios, si su distribucion"
+    echo "no es Debian 9 y sigue la instrucciones de su distribucion en:"
+    echo "https://docs.mongodb.com/manual/administration/install-on-linux/"
+    read -p "Su distribucion es Debian? (s,n) " install
+    if echo "$install" | grep -iq "^s" ;then
+      echo Instalando mongodb...
+      echo "--------------------------------"
+      echo 'deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main' | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+      $CMD_UPDATE
+      $CMD_INSTALL_MONGODB;$CMD_INIT_SERVICE_MONGODB
+      echo "--------------------------------"
+    fi
+else
+    echo Omitimos mongodb
+fi
+
+CMD_INSTALL_YARN="sudo apt-get install -y yarn"
+
+echo "\n"
+echo "Se necesita instalar yarn, se ejecutara:"
+echo "$CMD_INSTALL_YARN"
+read -p "Deseas instalar yarn? (s,n) " install
+if echo "$install" | grep -iq "^s" ;then
+  echo Instalando yarn...
+  echo "--------------------------------"
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  $CMD_UPDATE
+  $CMD_INSTALL_YARN
+  if [ $? -ne 0 ]; then
+     echo "[ERROR] Un error ha ocurrido al ejecutar:"
+     echo "$CMD_INSTALL_YARN"
+     echo "Pruebe a copiar el comando y a continuar con la instalacion"
+     exit 3
+  fi
+  echo 'export PATH="$HOME/.yarn/bin:$PATH"' >> ~/.bashrc
+else
+    echo Omitimos yarn
+fi
+
+CMD_INSTALL_GEM="./bin/bundle install"
+CMD_UPDATE_GEM="./bin/bundle update"
+
+echo "\n"
+echo "Se necesita instalar las gemas y las dependencias de vuejs, se ejecutara:"
+echo "$CMD_INSTALL_GEM"
+echo "$CMD_UPDATE_GEM"
+read -p "Deseas instalar las gemas? (s,n) " install
+if echo "$install" | grep -iq "^s" ;then
+  echo Instalamos Gemas del proyecto...
+  echo "--------------------------------"
+  $CMD_INSTALL_GEM
+  $CMD_UPDATE_GEM
+  echo Descargando dependencias vuejs...
+  echo "--------------------------------"
+  cd app-front
+  yarn install
+fi
+
+echo "Se ha instalado todo correctamente."
+echo "Inicia los servidores:"
+echo "--------------------------------"
+echo "- Backend (RoR)"
+echo "    rails s"
+echo " El servidor estara corriendo en: http://localhost:3000"
+
+echo "- Front (VueJS)"
+echo "    cd app-front"
+echo "    yarn dev"
+echo " El servidor estara corriendo en: http://localhost:8080"
